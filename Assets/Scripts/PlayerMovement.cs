316b87d44a32c0196fdb76d850df8c1b194ec014
Hage <89595749+HageFX-78@users.noreply.github.com>
2024-08-03 04:20:34 +0800

    Cinemachine and some visual test to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameObject dragPoint;
     [SerializeField] private Rigidbody2D rb;
-
+    [SerializeField] private GameObject sprite;
 
 
     [SerializeField] private float dragDistance = 1.5f;
@@ -20,12 +20,13 @@ public class PlayerMovement : MonoBehaviour
 
     public bool isMoving = false;
     private Vector2 lastVelocity = Vector2.zero;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private IEnumerator spriteShake = null;
+    private IEnumerator squishAndSquash = null;
+    private Vector2 lastContactNormal = Vector2.zero;
+
+    // ------------------------------------------------------- Unity functions
+
     void Update()
     {
         lastVelocity = rb.velocity;
@@ -36,8 +37,6 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = Vector2.zero;
         }
     }
-
-    Vector2 difference = Vector2.zero;
     private void OnMouseDown()
     {
         //dragPoint.SetActive(true);
@@ -46,6 +45,13 @@ public class PlayerMovement : MonoBehaviour
     private void OnMouseDrag()
     {
         if (isMoving) return;
+
+        if(spriteShake == null)
+        {
+            spriteShake = ShakeSprite();
+            StartCoroutine(spriteShake);
+        }
+
         dragPoint.transform.position = 
             (Vector2)transform.position - Vector2.ClampMagnitude( 
                 (Vector2)transform.position - (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition)
@@ -56,23 +62,84 @@ public class PlayerMovement : MonoBehaviour
     private void OnMouseUp()
     {
         if (isMoving) return;
-        
-        //dragPoint.SetActive(false);
+
         rb.AddForce(-(Vector2)dragPoint.transform.localPosition * ((Vector2)dragPoint.transform.localPosition).magnitude * shootForce);
         dragPoint.transform.localPosition = Vector2.zero;
 
+        if(spriteShake != null)
+        {
+            StopCoroutine(spriteShake);
+            spriteShake = null;
+            sprite.transform.localPosition = Vector3.zero;
+        }
         Invoke("TriggerMoving", 0.1f);
         
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Vector2 direction = Vector2.Reflect(lastVelocity.normalized, other.contacts[0].normal);
+        // Bounce off the wall
+        lastContactNormal = other.contacts[0].normal;
+        Debug.Log(lastContactNormal);
+        Vector2 direction = Vector2.Reflect(lastVelocity.normalized, lastContactNormal);
         rb.velocity = direction * lastVelocity.magnitude * wallDamping;
+
+        if(squishAndSquash == null)
+        {
+            squishAndSquash = SquishAndSquash();
+            StartCoroutine(squishAndSquash);
+        }else
+        {
+            
+            StopCoroutine(squishAndSquash);
+            squishAndSquash = null;
+            ResetScale();
+
+            squishAndSquash = SquishAndSquash();
+            StartCoroutine(squishAndSquash);
+        }
     }
 
+
+    // ------------------------------------------------------- External functions
     private void TriggerMoving()
     {
         isMoving = true;
+    }
+    private void ResetScale()
+    {
+        sprite.transform.localScale = Vector2.one;
+    }
+
+    // ------------------------------------------------------- Coroutines
+    public IEnumerator ShakeSprite()
+    {
+        Vector3 originalPos = sprite.transform.position;
+        float shakeAmount = 0.05f;
+        while (true)
+        {
+            sprite.transform.position = originalPos + UnityEngine.Random.insideUnitSphere * shakeAmount;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    public IEnumerator SquishAndSquash()
+    {
+
+        float duration = 0.3f;
+        float currentTime = 0f; 
+        Vector2 flipDirection = new Vector2(
+            math.abs(lastContactNormal.x) > math.abs(lastContactNormal.y) ? 1 : math.abs(lastContactNormal.y) * 0.9f, 
+            math.abs(lastContactNormal.y) > math.abs(lastContactNormal.x) ? 1 : math.abs(lastContactNormal.x) * 0.9f);
+        // size sprite by 1.5 and go back to 1 with sine wave
+        while (currentTime < duration)
+        {
+            
+            sprite.transform.localScale = Vector2.Lerp(Vector2.one, Vector2.one * flipDirection, Mathf.Sin(currentTime / duration * Mathf.PI));
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        ResetScale();
+        squishAndSquash = null;
     }
 }
